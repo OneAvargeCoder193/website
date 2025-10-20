2627195b166497230b03d947027009e581d24eeb
OneAvargeCoder193 <mgiakimenko@outlook.com>
2025-10-20 15:14:32 -0400

    base

---
+++ script.ts
@@ -0,0 +1,13 @@
+const skibidi = document.getElementById("skibidi")!;
+let angle = 0;
+let scale = 1;
+
+setInterval(() => {
+    skibidi.style.transform = "rotate(" + angle + "deg)";
+    angle += 0.1;
+    skibidi.style.transform += "scale(" + scale + ")";
+    scale = (scale - 1) * 0.95 + 1;
+}, 1);
+skibidi.addEventListener("click", () => {
+    scale += 0.25;
+});
